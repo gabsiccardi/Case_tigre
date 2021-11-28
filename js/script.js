4976739e0aa77da3000c82027825240565b6db7e
Gabriel <gab.siccardi@gmail.com>
2021-11-28 16:41:31 -0300

    fgfg

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var clicks = 0;
-
-        function botao_click(){
-            clicks++;
-            document.querySelector("#clicks").innerHTML = "Você clicou " +clicks+ " vezes no botão."
-        }
